a9d377b08d3fcb5953e4c312688b4b31fa13ddf2
Tom Steiner <stom@Toms-MacBook-Air.local>
2019-12-11 14:08:08 +0100

    added ServiceList and ServiceCard component

---
+++ src/components/ServiceCard/ServiceCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ServiceCard.scss';
+
+export default ({title, image, color}) => (
+
+    <li className={"cards__list__card cards__list__card--" + color}>
+        <a className="cards__list__card__link" href="#">
+            <p className="cards__list__card__headline">{title}</p>
+            {image}
+        </a>
+    </li>
+
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/Seo/seo";
+import ServiceList from '../components/ServiceList/ServiceList';
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
+      <ServiceList/>
   </Layout>
 )
 
