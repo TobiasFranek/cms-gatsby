ba79568fbfff523457fb07cdf2d261992005c024
Simon Seebacher <sseebacher.mmt-b2018@fh-salzburg.ac.at>
2019-12-11 14:21:48 +0100

    change class to className

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './News.scss';
 
 export default () => (
-    <ul class="news-list">
-        <li class="news-list__item">
-            <a class="a" href="#">365 Postkarten</a><span class="p"> - Eine Liebeserklärung für
-            jeden Tag! </span><a class="a" href="#">[mehr erfahren]</a>
+    <ul className="news-list">
+        <li className="news-list__item">
+            <a className="a" href="#">365 Postkarten</a><span className="p"> - Eine Liebeserklärung für
+            jeden Tag! </span><a className="a" href="#">[mehr erfahren]</a>
         </li>
-        <li class="news-list__item">
-            <a class="a" href="#">Award-Nominierung</a> <span class="p">- Tolle News: ich bin
-            bei der Endauswahl 2018! </span><a class="a" href="#">[mehr erfahren]</a>
+        <li className="news-list__item">
+            <a className="a" href="#">Award-Nominierung</a> <span className="p">- Tolle News: ich bin
+            bei der Endauswahl 2018! </span><a className="a" href="#">[mehr erfahren]</a>
         </li>
-        <li class="news-list__item">
-            <a class="a" href="#">CMYK erklärt</a><span class="p"> - der neuste</span>
-            <a class="a" href="#">Blog</a><span class="p">-Eintrag! </span>
-            <a class="a" href="#">[mehr erfahren]</a>
+        <li className="news-list__item">
+            <a className="a" href="#">CMYK erklärt</a><span className="p"> - der neuste</span>
+            <a className="a" href="#">Blog</a><span className="p">-Eintrag! </span>
+            <a className="a" href="#">[mehr erfahren]</a>
         </li>
     </ul>
 )
