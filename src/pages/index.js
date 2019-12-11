d7c4f1c14a49868c15c4dd89703581d96250bd0c
Simon Seebacher <sseebacher.mmt-b2018@fh-salzburg.ac.at>
2019-12-11 13:56:30 +0100

    add References as 1 component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import Layout from "../components/Layout/Layout";
+import References from "../components/References/References";
 import SEO from "../components/Seo/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
+    <References />
   </Layout>
 )
 
